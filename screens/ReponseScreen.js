9420a9094d5703b84b5795dd9b9a71a94be2c0af
wassimmm <wassim.hmouda@esprit.tn>
2024-02-19 11:46:03 +0100

    update reponse

--- screens/ReponseScreen.js
+++ screens/ReponseScreen.js
@@ -1,30 +1,83 @@
-// AlbumScreen.js
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import CustomHeader from '../components/CustomHeader'; // Update the path
-const ReponseScreen = () => {
-    return (
-        <View style={styles.container}>
-          <Text>Reponse Screen</Text>
-          <View style={styles.footer}>
-            <CustomHeader />
-          </View>
-        </View>
-      );
-    };
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      footer: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-      },
-    });
-    
-    export default ReponseScreen;
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import CustomHeader from '../components/CustomHeader';
+import { useNavigation } from '@react-navigation/native';
+
+const ReponseScreen = ({ route }) => {
+  const [conformite, setConformite] = useState(undefined);
+  const [commentaire, setCommentaire] = useState(undefined);
+  const { questionId } = route.params;
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // Fetch data or handle initial setup if needed
+  }, []);
+
+  const updateReponse = async () => {
+    console.warn(conformite, commentaire);
+    const url = `http://10.0.2.2:8000/api/updatereponse/${questionId}`;
+    try {
+      const response = await fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ conformite, commentaire })
+      });
+      if (!response.ok) {
+        throw new Error('Failed to update response');
+      }
+      const result = await response.json();
+      console.warn(result);
+    } catch (error) {
+      console.error('Error updating response:', error.message);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Conformite"
+        value={conformite}
+        onChangeText={(text) => setConformite(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Commentaire"
+        value={commentaire}
+        onChangeText={(text) => setCommentaire(text)}
+      />
+      <View style={{ marginBottom: 15 }}>
+        <Button onPress={updateReponse} title="Update Response" />
+      </View>
+      <Button onPress={() => navigation.goBack()} title="Close" />
+      <View style={styles.footer}>
+        <CustomHeader />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+});
+
+export default ReponseScreen;
