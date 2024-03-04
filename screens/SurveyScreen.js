4d0a9794164bf2b8fb5f31fb6bcdc81dfb9baa9e
wassimmm <wassim.hmouda@esprit.tn>
2024-03-04 12:04:56 +0100

    sorting projects

--- screens/SurveyScreen.js
+++ screens/SurveyScreen.js
@@ -1,53 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar, Image } from 'react-native';
-import CustomHeader from '../components/CustomHeader';
+import { View, Text, StyleSheet, Image, StatusBar ,ScrollView} from 'react-native';
 import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
 import CustomButton from '../components/CustomButton';
+import { useNavigation } from '@react-navigation/native';
 const SurveyScreen = ({ route }) => {
   const [customer, setCustomer] = useState(null);
+  const [projects, setProjects] = useState([]);
   const navigation = useNavigation();
+  const [sortedProjects, setSortedProjects] = useState([]);
+  const [sortByYear, setSortByYear] = useState(false);
   const { customerId } = route.params;
 
-  const handletheMissingInspectionsPress = () => {
-    // Implement logic for handling Missing Inspections button press
-    console.log('Button pressed');
-    
-    // Navigate to SitesScreen
-    navigation.navigate('SitesScreen', { customerId: customer.id });
-  };
-  
   useEffect(() => {
     async function fetchCustomer() {
       try {
-        console.log('Customer ID:', customerId); // Log the customerId
         const response = await axios.get(`http://10.0.2.2:8000/api/customer/${customerId}`);
-        console.log('Response data:', response.data); // Log the response data
-        setCustomer(response.data); // Set customer data from the response
+        if (response.status === 200) {
+          setCustomer(response.data);
+        } else {
+          console.log('Failed to fetch customer data');
+        }
+  
+        const responseProject = await axios.get(`http://10.0.2.2:8000/api/projects/${customerId}`);
+        const fetchedProjects = responseProject.data.projects;
+        setProjects(fetchedProjects);
+        setSortedProjects([...fetchedProjects]); // Initialize sortedProjects with fetched projects
       } catch (error) {
-        console.log('Error fetching customer data:', error); // Log any errors
+        console.log('Error fetching customer data:', error);
       }
     }
   
     fetchCustomer();
   }, [customerId]);
+  
+
+  const handletheMissingInspectionsPress = () => {
+    console.log('Button pressed');
+    // Implement navigation logic
+    navigation.navigate('SitesScreen', { customerId: customer.id });
+
+  };
+  const handleSortByYear = () => {
+    const sorted = [...projects]; // Copy projects array
+    console.log('Unsorted projects:', sorted); // Log unsorted projects
+    if (sortByYear) {
+      // Sort in ascending order
+      sorted.sort((a, b) => a.year.localeCompare(b.year));
+      console.log('Sorted projects (ascending):', sorted); // Log sorted projects
+    } else {
+      // Sort in descending order
+      sorted.sort((a, b) => b.year.localeCompare(a.year));
+      console.log('Sorted projects (descending):', sorted); // Log sorted projects
+    }
+    setSortedProjects(sorted);
+    setSortByYear(!sortByYear);
+  };
+  
+   
   return (
     <View style={styles.container}>
-     
       {customer && customer.Logo ? (
         <View style={styles.logoContainer}>
           <Image
             source={{ uri: `http://10.0.2.2:8000/assets/${customer.Logo}` }}
             style={styles.logo}
-            onLoadStart={() => console.log('Image loading started')}
-            onLoadEnd={() => console.log('Image loading ended')}
-            onError={(error) => console.log('Image loading error:', error)}
           />
         </View>
       ) : (
         <Text>No logo available</Text>
       )}
       {customer && (
+              <ScrollView style={styles.scrollView}>
         <View style={styles.card}>
           <View style={styles.detailsContainer}>
             <View style={styles.detailRow}>
@@ -55,20 +78,41 @@ const SurveyScreen = ({ route }) => {
                 <Text style={styles.detailLabel}>SN:</Text>
                 <Text style={styles.detailValue}>{customer.SN}</Text>
               </View>
-              {/* Add other rows of fields here */}
             </View>
             <CustomButton title="Join Survey Project" onPress={handletheMissingInspectionsPress} />
           </View>
-        </View>
+          <CustomButton
+                title={sortByYear ? 'Sort Ascending' : 'Sort Descending'}
+                onPress={handleSortByYear}
+              />
+           <Text style={styles.infoText}>Associated projects :</Text>
+      {sortedProjects.length > 0 ? (
+        sortedProjects.map((project, index) => (
+          <View key={index} style={styles.card}>
+            <View style={styles.detailsContainer}>
+              <View style={styles.detailRow}>
+                <View style={styles.detailItem}>
+                  <Text style={styles.detailLabel}>Project Name:</Text>
+                  <Text style={styles.detailValue}>{project.Nom}</Text>
+                </View>
+                <View style={styles.detailItem}>
+                  <Text style={styles.detailLabel}>Project Year:</Text>
+                  <Text style={styles.detailValue}>{project.year}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        ))
+      ) : (
+        <Text>No projects available</Text>
       )}
-       <Text style={styles.infoText}>associated pojects :</Text>
+        </View>
+        </ScrollView>
+      )}
+     
       <StatusBar style="auto" />
-      <View style={styles.footer}>
-        <CustomHeader />
-      </View>
     </View>
   );
-  
 };
 
 const styles = StyleSheet.create({
@@ -82,21 +126,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 20,
   },
+  scrollView: {
+    flex: 1,
+    width: '100%',
+  },
   logo: {
     width: 100,
     height: 100,
-    borderRadius: 50, // to make it circular
+    borderRadius: 50,
   },
   card: {
     backgroundColor: '#fff',
-    padding: 20,
     borderRadius: 10,
-    elevation: 3,
-    shadowOffset: { width: 0, height: 2 },
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    marginBottom: 20,
-    width: '80%', // Adjust the width as needed
+    elevation: 5,
+    width: '100%',
+    marginTop: 20,
   },
   detailsContainer: {
     marginTop: 20,
@@ -127,23 +180,6 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     textAlign: 'center',
     color: 'green',
-  },
-  button: {
-    backgroundColor: '#007bff',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  footer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
   },
 });
 
