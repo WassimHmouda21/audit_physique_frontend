a7fd7b984e183e97b794445db1976456d136a356
wassimmm <wassim.hmouda@esprit.tn>
2024-03-04 09:33:56 +0100

    logo sites

--- screens/SitesScreen.js
+++ screens/SitesScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, StatusBar, Image ,ScrollView } from 'react-native';
 import CustomHeader from '../components/CustomHeader';
 import axios from 'axios';
 
@@ -16,19 +16,28 @@ class Customer_site {
 const SitesScreen = ({ route, navigation }) => {
   const { customerId } = route.params; // Get customerId from route params
   const [customerSites, setCustomerSites] = useState([]);
+  const [customer, setCustomer] = useState(null);
 
   useEffect(() => {
     async function fetchCustomerSites() {
       try {
         const response = await axios.get(`http://10.0.2.2:8000/api/customer_sites/${customerId}`);
         console.log(response.data);
+        
         if (response.data.status === 200) {
           // Map the response data to instances of Customer_site class
           const sites = response.data.customer_sites.map(site => new Customer_site(site.id, site.Numero_site, site.Structure, site.Lieu, site.Customer_Id));
           setCustomerSites(sites);
+         
         } else {
           console.log(response.data.message); // Log error message if status is not 200
         }
+
+        const responseCustomer = await axios.get(`http://10.0.2.2:8000/api/customer/${customerId}`);
+        console.log('Response data:', responseCustomer.data); // Log the response data
+        setCustomer(responseCustomer.data); // Set customer data from the response
+
+
       } catch (error) {
         console.log(error);
       }
@@ -36,6 +45,11 @@ const SitesScreen = ({ route, navigation }) => {
 
     fetchCustomerSites();
   }, [customerId]); // Include customerId in dependency array to re-fetch data when it changes
+
+
+  
+  
+
 
   const handleCardPress = (customerSite) => {
     // Log the customerSite object to inspect its structure and properties
@@ -49,16 +63,30 @@ const SitesScreen = ({ route, navigation }) => {
 
   return (
     <View style={styles.container}>
+    {customer && customer.Logo ? (
+      <View style={styles.logoContainer}>
+        <Image
+          source={{ uri: `http://10.0.2.2:8000/assets/${customer.Logo}` }}
+          style={styles.logo}
+          onLoadStart={() => console.log('Image loading started')}
+          onLoadEnd={() => console.log('Image loading ended')}
+          onError={(error) => console.log('Image loading error:', error)}
+        />
+      </View>
+    ) : (
+      <Text>No logo available</Text>
+    )}
       <ScrollView style={styles.scrollView}>
         <View style={styles.card}>
-          <View style={styles.cardContent}>
+        <Text style={styles.infoText}>Select Customer Site:</Text>
+          {/* <View style={styles.cardContent}>
             <Text style={styles.infoTextet}>Audit de Conformité ISO 27002 / Rapport d’Audit de Vérification des Aspects de Sécurité Physique</Text>
             <Text style={styles.infoTexte}>Référence normative : ISO 27002 (Chapitre A.11)
               Méthode : MEHARI 2017 (Sécurité des Sites et locaux informatiques)
             </Text>
-          </View>
+          </View> */}
         </View>
-        <Text style={styles.infoText}>Select Customer Site:</Text>
+       
         {customerSites.length > 0 ? (
           customerSites.map((site, index) => (
             <TouchableOpacity key={index} style={styles.card} onPress={() => handleCardPress(site)}>
@@ -78,10 +106,10 @@ const SitesScreen = ({ route, navigation }) => {
                   <Text style={[styles.detailLabel, { color: 'blue' }]}>Lieu:</Text>
                   <Text style={[styles.detailValue]}>{site.Lieu}</Text>
                 </View>
-                <View style={styles.detailRow}>
+                {/* <View style={styles.detailRow}>
                   <Text style={[styles.detailLabel, { color: 'blue' }]}>Customer_Id:</Text>
                   <Text style={[styles.detailValue]}>{site.Customer_Id}</Text>
-                </View>
+                </View> */}
               </View>
             </TouchableOpacity>
           ))
@@ -110,6 +138,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     textAlign: 'center',
     color: 'green',
+    marginLeft: 30
   },
   infoTexte: {
     fontSize: 17,
@@ -132,6 +161,15 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingHorizontal: 10,
     paddingVertical: 15,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    borderRadius: 50, // to make it circular
   },
   rectangle: {
     width: 20,
